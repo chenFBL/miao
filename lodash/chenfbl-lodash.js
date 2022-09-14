00877c1779c24c565ff6700d07433cfc388c4cbd
chengang <chengang@qq.com>
2022-09-14 21:03:01 +0800

    3

--- lodash/chenfbl-lodash.js
+++ lodash/chenfbl-lodash.js
@@ -7,4 +7,14 @@ var chenfbl = {
     return temp
   }
 
+  compact: function (ary) {
+    var result = []
+    for (var i = 0; i < ary.length; i++) {
+      if (ary[i]) {
+        result.push(ary[i])
+      }
+    }
+    return result
+  }
+
 }
