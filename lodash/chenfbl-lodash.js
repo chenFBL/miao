a9a180edb51a19508c771c32c9d63efeafb5eb91
chengang <chengang@qq.com>
2022-09-14 20:40:44 +0800

    2

--- lodash/chenfbl-lodash.js
+++ lodash/chenfbl-lodash.js
@@ -1,7 +1,10 @@
-function chunk(ary, n) {
-  var temp = []
-  for (var i = 0; i < ary.length; i += n) {
-    temp.push(ary.slice(i, i + n))
+var chenfbl = {
+  chunk: function(ary, n) {
+    var temp = []
+    for (var i = 0; i < ary.length; i += n) {
+      temp.push(ary.slice(i, i + n))
+    }
+    return temp
   }
-  return temp
+
 }
