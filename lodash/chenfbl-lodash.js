e3ae56a855ed269bfc95d7d1a96842cccdfd386d
chengang <chengang@qq.com>
2022-09-15 11:19:38 +0800

    js5

--- lodash/chenfbl-lodash.js
+++ lodash/chenfbl-lodash.js
@@ -17,8 +17,11 @@ var chenfbl = {
     return result
   },
 
-  fill: function () {
-
+  fill: function (array, value, start=0, end=array.length) {
+    for (var i = start; i < end; i++) {
+      array[i] = value
+    }
+    return array
   },
 
   drop: function(array, n = 1) {
@@ -29,7 +32,49 @@ var chenfbl = {
     return result
   },
 
-  findIndex: function () {
+  findIndex: function(array, predicate, fromIndex=0) {
 
+  },
+
+  identity: function (value) {
+    return value
+  },
+
+  dropRight: function (array, n = 1) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      result.push(array[i])
+    }
+    return result
+  },
+
+  last: function(array) {
+    return array[array.length - 1]
+  },
+
+  indexOf: function(array, value, fromIndex = 0) {
+    for (var i = fromIndex; i < array.length; i++) {
+      if (array[i] === value) {
+        return i
+      }
+    }
+    return -1
+  },
+
+  head: function (array) {
+    return array[0] ? array[0] : undefined;
+  },
+
+  lastIndexOf: function (array, value, fromIndex = array.length - 1) {
+    for (var i = fromIndex; i > 0; i--) {
+      if (array[i] === value) {
+        return i
+      }
+    }
+    return -1
+  },
+
+  nth: function (array, n = 0) {
+    return array.at(n)
   }
 }
